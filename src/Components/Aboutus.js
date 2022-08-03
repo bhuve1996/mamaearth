560b1cbe46f098bab532115c22eb6a689b7bf226
Deepak <aroradeepak485@gmail.com>
2022-08-04 02:56:23 +0530

    aboutus

--- src/Components/Aboutus.js
+++ src/Components/Aboutus.js
@@ -1,5 +1,29 @@
 function Aboutus() {
-  return <>Aboutus</>;
+  return <>
+    <section>
+        <div className="me-banner">
+          <div className="me-banner-left">
+          <div className="me-aboutus-left-inner-container">
+          <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSep02FE82d4gAX79YAS6CEQ5soLJiX_VCq98rHo5fHzylJ-KA/viewform?embedded=true" max-width="640" max-height="650" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+
+              
+            </div>
+
+            
+          </div>
+          <div className="me-banner-right">
+            
+            <div className="me-aboutus-right-inner-container">
+              <h2>Our Goodness Promise</h2>
+              
+              <p>At Mamaearth, we live to spread a little love and goodness every day. We believe that goodness isn't a superpower, nor a special gift, it's inside all of us and it shows in the little choices we make. Our mission is to bring you the best of nature through our purest and most nurturing products that are made without any toxins or harmful chemicals. </p>
+
+              <button className="button button-home-banner">SHOP NOW</button>
+            </div>
+          </div>
+        </div>
+      </section>
+  </>;
 }
 
 export default Aboutus;
