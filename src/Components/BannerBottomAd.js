a4dbbbb177893a7036561429915df1df12f516a3
Bhuvesh <bhuve1996@gmail.com>
2022-08-05 20:16:27 +0530

    changes

--- src/Components/BannerBottomAd.js
+++ src/Components/BannerBottomAd.js
@@ -8,49 +8,49 @@ function BannerAd() {
               <ul>
                 <li>
                   <div className="me-banner-ad-container-list">
-                    <i class="fa fa-eercast" aria-hidden="true"></i>
+                    <i className="fa fa-eercast" aria-hidden="true"></i>
                     <p>Baby</p>
                   </div>
                 </li>
                 <li>
                   <div className="me-banner-ad-container-list">
-                    <i class="fa fa-eercast" aria-hidden="true"></i>
+                    <i className="fa fa-eercast" aria-hidden="true"></i>
                     <p>Beauty</p>
                   </div>
                 </li>
                 <li>
                   <div className="me-banner-ad-container-list">
-                    <i class="fa fa-eercast" aria-hidden="true"></i>
+                    <i className="fa fa-eercast" aria-hidden="true"></i>
                     <p>Hair</p>
                   </div>
                 </li>
                 <li>
                   <div className="me-banner-ad-container-list">
-                    <i class="fa fa-eercast" aria-hidden="true"></i>
+                    <i className="fa fa-eercast" aria-hidden="true"></i>
                     <p>Face</p>
                   </div>
                 </li>
                 <li>
                   <div className="me-banner-ad-container-list">
-                    <i class="fa fa-eercast" aria-hidden="true"></i>
+                    <i className="fa fa-eercast" aria-hidden="true"></i>
                     <p>Body</p>
                   </div>
                 </li>
                 <li>
                   <div className="me-banner-ad-container-list">
-                    <i class="fa fa-eercast" aria-hidden="true"></i>
+                    <i className="fa fa-eercast" aria-hidden="true"></i>
                     <p>Gift</p>
                   </div>
                 </li>
                 <li>
                   <div className="me-banner-ad-container-list">
-                    <i class="fa fa-eercast" aria-hidden="true"></i>
+                    <i className="fa fa-eercast" aria-hidden="true"></i>
                     <p>All Products</p>
                   </div>
                 </li>
                 <li>
                   <div className="me-banner-ad-container-list">
-                    <i class="fa fa-eercast" aria-hidden="true"></i>
+                    <i className="fa fa-eercast" aria-hidden="true"></i>
                     <p>Plant Goodness</p>
                   </div>
                 </li>
