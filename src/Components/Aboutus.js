3b11861008c7a6d50b3f2a56b1f4a628a319929d
Bhuvesh <bhuve1996@gmail.com>
2022-08-05 20:09:05 +0530

    css

--- src/Components/Aboutus.js
+++ src/Components/Aboutus.js
@@ -1,58 +1,71 @@
+import ContactForm from "./ContactForm";
+import ContactFormNew from "./ContactFormNew";
+import IframeResizer from 'iframe-resizer-react'
+import React ,{useState,useEffect}from "react";
 function Aboutus() {
-  return <>
-    <section>
+  // const [frameHeight, setFrameHeight] = useState();
+
+
+ 
+  // useEffect(() => {
+  //   const frame = document.getElementById("myFrame");
+  //   console.log(
+  //     "height",
+  //     frame.contentWindow.document.body.scrollHeight + "px"
+  //   );
+
+  //   setTimeout(() => {
+  //     setFrameHeight(frame.contentWindow.document.body.scrollHeight + "px");
+  //   }, 100);
+  // }, []);
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     const iframeNew = document.getElementById('myFrame')
+  //     console.log(iframeNew.contentWindow.document)
+  //     // console.log(iframeNew?.contentDocument)
+  //   }, 5000);
+  //   return () => clearTimeout(timer);
+  // }, []);
+  return (
+    <>
+      <section>
         <div className="me-banner">
-          <div className="me-aboutus-left">
-          <div className="me-aboutus-left-inner-container">
-          {/* <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSep02FE82d4gAX79YAS6CEQ5soLJiX_VCq98rHo5fHzylJ-KA/viewform?embedded=true" max-width="640" max-height="650" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe> */}
-          <form
-        action="https://docs.google.com/forms/d/e/1FAIpQLSep02FE82d4gAX79YAS6CEQ5soLJiX_VCq98rHo5fHzylJ-KA/formResponse"
-        method="post">
-        <label htmlFor="me-name">Name :</label>
-        <div>
-        <input type="text" placeholder="Name*" name="entry.2005620554" required/></div>
-        
-        <label htmlFor="me-email">Email Address :</label>
-        <div>
-        <input type="email" placeholder="Email address*" name="entry.1045781291" required/></div>
-        
-        <label htmlFor="me-address">Address :</label>
-        <div>
-        <input type="text" placeholder="Address" name="entry.1065046570"/>
-        </div>
-        
-        <label htmlFor="me-phone">Phone No :</label>
-        <div>
-        <input type="text" placeholder="Phone No" name="entry.1166974658"/></div>
-        
-        <label htmlFor="me-comments">Comments :</label>
-        <div>
-        <input type="text" placeholder="Comments" name="entry.839337160"/>
-        </div>
-        
-       {/* <label>Message*</label>
-        <textarea rows="5" placeholder="Message*" name="entry.1166974658" required></textarea>  */}
-
-        <button type="submit" className="me-btn-form">Send</button>
-    </form>
-              
+          <div className="me-banner-left">
+            <div className="me-aboutus-left-inner-container">
+              {/* <iframe
+                src="https://docs.google.com/forms/d/e/1FAIpQLSep02FE82d4gAX79YAS6CEQ5soLJiX_VCq98rHo5fHzylJ-KA/viewform?embedded=true"
+                id="myFrame"
+                width="100%" 
+                height="1100px"
+                frameBorder="0"
+                scrolling="no"
+              >
+                Loading…
+              </iframe> */}
+              {/* <ContactForm /> */}
+              <ContactFormNew/>
             </div>
-
-            
           </div>
-          <div className="me-aboutus-right">
-            
+          <div className="me-banner-right">
             <div className="me-aboutus-right-inner-container">
               <h2>Our Goodness Promise</h2>
-              
-              <p>At Mamaearth, we live to spread a little love and goodness every day. We believe that goodness isn't a superpower, nor a special gift, it's inside all of us and it shows in the little choices we make. Our mission is to bring you the best of nature through our purest and most nurturing products that are made without any toxins or harmful chemicals. </p>
+
+              <p>
+                At Mamaearth, we live to spread a little love and goodness every
+                day. We believe that goodness isn't a superpower, nor a special
+                gift, it's inside all of us and it shows in the little choices
+                we make. Our mission is to bring you the best of nature through
+                our purest and most nurturing products that are made without any
+                toxins or harmful chemicals.{" "}
+              </p>
 
               <button className="button button-home-banner">SHOP NOW</button>
             </div>
           </div>
         </div>
       </section>
-  </>;
+    </>
+  );
 }
 
 export default Aboutus;
